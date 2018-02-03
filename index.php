1793274bda0ce919800cdb293c26bbe3e5c1b6cc
hiro <h.hayashi@miraito-inc.co.jp>
2018-02-04 04:21:41 +0900

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/default.css">
+    <title>株式会社ミライトデザイン</title>
+</head>
+<body>
+    <div id="content">
+        <div id="logo">
+            <img src="images/logo.png" alt="会社ロゴ">
+        </div>
+        <div id="logo-word">
+            <img src="images/logo-word.png" alt="会社ロゴテキスト">
+        </div>
+    </div>
+</body>
+</html>
