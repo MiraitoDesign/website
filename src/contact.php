32a06bc1dc3cb4cd5bfbfdc06b04fccaaa756598
YasushiUdagawa <y.udagawa@miraito-inc.co.jp>
2018-02-21 00:04:30 +0900

    refactor: メール送信先を会社代表メールアドレスに変更

--- src/contact.php
+++ src/contact.php
@@ -3,8 +3,7 @@
 mb_language("Japanese");
 mb_internal_encoding("UTF-8");
 
-$to      = 'h.hayashi@miraito-inc.co.jp' . ', ';
-$to      .= 'y.udagawa@miraito-inc.co.jp';
+$to      = 'info@miraito-inc.co.jp';
 $subject = 'HPからの問い合わせ';
 $message = $_POST['name'] . "\r\n" . $_POST['message'];
 $headers = 'From: ' . $_POST['email'] . "\r\n";
