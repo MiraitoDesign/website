f265a55b035eabaf106d6e829480a08d724a766d
YasushiUdagawa <y.udagawa@miraito-inc.co.jp>
2018-03-02 07:38:56 +0900

    feat: CSS/JavaScriptでの動き等を仮実装

--- js/application.js
+++ js/application.js
@@ -12,6 +12,7 @@ $(function(){
         }
     });
 
+/*
     $('div#mail').on('click', ()=>{
         $('#overlay, #contact').fadeIn(1000);
     })
@@ -19,6 +20,7 @@ $(function(){
     $('#overlay').on('click', () => {
         $('#overlay, #contact').fadeOut();
     })
+*/
 
     $('#send').on('click', (e) => {
         e.preventDefault();
@@ -43,7 +45,53 @@ $(function(){
         })
     })
 
+    $('#logo-word img').on('click', ()=>{
+        $('#golden-ratio').toggle('display');
+    });
 
+    $('.open-detail-content').on('click', ()=>{
+        $('#home-content').animate({left:"20px"});
+        $('#detail-content').animate({left:"90px", opacity:"show"});
+        $('#bar-line').animate({left:"50px", top:"350px", deg: 90}, {
+            duration: 500,
+            progress: function (){
+                $('#bar-line').css({transform:'rotate(' + this.deg + 'deg)'})
+            }
+        });
+    });
+
+    $('.close-detail-content').on('click', ()=>{
+        $('#home-content').removeClass('fixed');
+        $('#home-content').animate({left:"350px"});
+        $('#detail-content').animate({left:"700px", opacity:"hide"});
+        $('#detail-company').animate({opacity:"hide"});
+        $('#detail-service').animate({opacity:"hide"});
+        $('#detail-contact').animate({opacity:"hide"});
+        $('#bar-line').animate({left:"200px", top:"420px", deg: 0}, {
+            duration: 500,
+            progress: function (){
+                $('#bar-line').css({transform:'rotate(' + this.deg + 'deg)'})
+            }
+        });
+    });
+
+    $('#company').on('click', ()=>{
+        $('#detail-company').animate({opacity:"show"});
+        $('#detail-service').animate({opacity:"hide"});
+        $('#detail-contact').animate({opacity:"hide"});
+    });
+
+    $('#service').on('click', ()=>{
+        $('#detail-company').animate({opacity:"hide"});
+        $('#detail-service').animate({opacity:"show"});
+        $('#detail-contact').animate({opacity:"hide"});
+    });
+
+    $('#mail').on('click', ()=>{
+        $('#detail-company').animate({opacity:"hide"});
+        $('#detail-service').animate({opacity:"hide"});
+        $('#detail-contact').animate({opacity:"show"});
+    });
 
     locateCenter();
     $(window).resize(locateCenter);
@@ -89,3 +137,4 @@ const locateCenter = () => {
         'top': ((h - ch) / 2) + 'px'
     });
 }
+
